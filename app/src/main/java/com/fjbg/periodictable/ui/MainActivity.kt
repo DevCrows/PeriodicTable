d4ed5dbe715c0e4266b270887fefd536c463b08b
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-05-10 13:53:50 +0700

    full screen fixed

--- app/src/main/java/com/fjbg/periodictable/ui/MainActivity.kt
+++ app/src/main/java/com/fjbg/periodictable/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fjbg.periodictable.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.fjbg.periodictable.ui.theme.PeriodicTableTheme
@@ -10,6 +12,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		
+		window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+		window.statusBarColor = Color.TRANSPARENT
+		
 		setContent {
 			PeriodicTableTheme {
 			
